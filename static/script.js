9a1ec16e4a545febb3a3bad0263957b69abb2714
viral2618 <130451325+viral2618@users.noreply.github.com>
2025-11-11 19:08:09 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -13,25 +13,50 @@ ctx.lineWidth = 20;
 ctx.lineCap = "round";
 ctx.strokeStyle = "black";
 
+function getPos(e) {
+  const rect = canvas.getBoundingClientRect();
+  if (e.touches) { 
+    return {
+      x: e.touches[0].clientX - rect.left,
+      y: e.touches[0].clientY - rect.top,
+    };
+  } else { 
+    return {
+      x: e.offsetX,
+      y: e.offsetY,
+    };
+  }
+}
+
 function startPosition(e) {
+  e.preventDefault();
   painting = true;
   draw(e);
 }
-function endPosition() {
+function endPosition(e) {
+  e.preventDefault();
   painting = false;
   ctx.beginPath();
 }
 function draw(e) {
+  e.preventDefault();
   if (!painting) return;
-  ctx.lineTo(e.offsetX, e.offsetY);
+  const pos = getPos(e);
+  ctx.lineTo(pos.x, pos.y);
   ctx.stroke();
   ctx.beginPath();
-  ctx.moveTo(e.offsetX, e.offsetY);
+  ctx.moveTo(pos.x, pos.y);
 }
+
 
 canvas.addEventListener("mousedown", startPosition);
 canvas.addEventListener("mouseup", endPosition);
 canvas.addEventListener("mousemove", draw);
+
+
+canvas.addEventListener("touchstart", startPosition);
+canvas.addEventListener("touchend", endPosition);
+canvas.addEventListener("touchmove", draw);
 
 clearBtn.addEventListener("click", () => {
   ctx.fillStyle = "white";
